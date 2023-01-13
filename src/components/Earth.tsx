038f61e838904c2e250632e23858b03651652ff1
johncrossrollon <juanrolon54@outlook.com>
2023-01-13 01:47:31 -0300

    fixed typescript bug (IT WAS A BUG!!!!)

--- src/components/Earth.tsx
+++ src/components/Earth.tsx
@@ -1,28 +1,30 @@
 import * as THREE from 'three'
 import React, { useEffect, useLayoutEffect, useRef, useState } from 'react'
 import { Canvas, useFrame, ThreeElements, useLoader } from '@react-three/fiber'
-import { a } from '@react-spring/three'
+import { a, animated } from '@react-spring/three'
 import { useSpring } from '@react-spring/core'
+import { useTexture } from '@react-three/drei'
 
 import { useContext } from '../context/AppContext'
-import { useTexture } from '@react-three/drei'
 import earthImg from '../assets/earthImg.jpg'
 import earthNormal from '../assets/earthNormal.jpg'
 
-function Box(props: ThreeElements['mesh']) {
+function Sphere(props: ThreeElements['mesh']) {
   const texture = useTexture(earthImg)
   const normalTexture = useTexture(earthNormal)
   return (
-    <a.mesh {...props}>
+    <mesh {...props}>
       <sphereGeometry args={[2, 100, 100]} />
       <meshStandardMaterial
         map={texture}
         normalMap={normalTexture}
         color={'lightgray'}
       />
-    </a.mesh>
+    </mesh>
   )
 }
+
+const AnimatedSphere = animated(Sphere)
 
 export default () => {
   const degToRad = (deg: number) => (deg * Math.PI) / 180.0
@@ -43,11 +45,10 @@ export default () => {
     <Canvas>
       <pointLight position={[20, 50, 60]} />
       <ambientLight intensity={0.3} />
-      <Box
+      <AnimatedSphere
         position={[0, 0, 0]}
         rotation-y={springY}
         rotation-x={springX}
-        castShadow
       />
     </Canvas>
   )
